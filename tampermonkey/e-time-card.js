8841b8d96d6b7c17257faedbc5aeded000e3790b
jobscale <jobscale@users.noreply.github.com>
2023-11-16 09:48:39 +0900

    tampermonkey script added

---
+++ tampermonkey/e-time-card.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         e-timeCard
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://e-timecard.ne.jp/s/EPSINP*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=e-timecard.ne.jp
+// @grant        none
+// ==/UserScript==
+
+setTimeout(() => {
+  Array.from(document.querySelectorAll('[id^="workPlaceKbnHome"]'))
+  .forEach(el => { el.checked = true; });
+
+  const el = document.querySelector('table .cmWidthP04');
+  el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}, 100);
+
