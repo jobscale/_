19c99660418f231ca5017a052f99d2cf6419da0c
jobscale <jobscale@users.noreply.github.com>
2025-10-07 23:19:37 +0900

    loop milliseconds updated

--- bin/loop.js
+++ bin/loop.js
@@ -14,11 +14,13 @@ const play = () => {
   });
 };
 
+const now = () => Math.floor(performance.now());
+
 const enter = (endTime, step) => new Promise(async resolve => {
-  const start = Date.now();
+  const start = performance.now();
   const tick = 1000 / step;
   while (true) {
-    const left = endTime - Date.now();
+    const left = endTime - now();
     if (left <= 0) break;
     const remaining = left / 1000;
     if (step === 1) process.stdout.write(`\r${remaining.toFixed(0).padStart(5, ' ')} `);
@@ -26,21 +28,28 @@ const enter = (endTime, step) => new Promise(async resolve => {
     const nextTick = Math.ceil((left % 1000) % tick) || tick;
     await new Promise(res => { setTimeout(res, nextTick); });
   }
-  process.stdout.write(`\r    0   (${(Date.now() - start) / 1000})\n`);
+  const benchmark = (performance.now() - start) / 1000;
+  process.stdout.write(`\r    0   (${benchmark.toFixed(3)})\n`);
   resolve();
 });
 
-const loop = async (seconds, step, mode = '') => {
-  const endTime = Date.now() + (seconds * 1000);
+const loop = async (seconds, step, useSound) => {
+  const endTime = now() + (seconds * 1000);
   await enter(endTime, step);
-  if (mode !== 'silent') play();
+  if (useSound) play();
 }
 
-const [,, secondsArg, mode, stepArg] = process.argv;
-const step = Math.min(20, Number.parseInt(stepArg, 10) || 1);
-const seconds = Number.parseInt(secondsArg, 10);
-if (!seconds) {
-  logger.error('Usage: loop.js <seconds> [silent]');
-  process.exit(1);
-}
-loop(seconds, step, mode);
+const main = argv => {
+  const timer = argv.filter(arg => arg !== '--silent');
+  const [secondsArg, stepArg] = timer;
+  const step = Math.min(20, Number.parseInt(stepArg, 10) || 1);
+  const seconds = Number.parseInt(secondsArg, 10);
+  if (!seconds) {
+    logger.error('Usage: loop.js <seconds> [silent]');
+    process.exit(1);
+  }
+  const useSound = argv.length === timer.length;
+  loop(seconds, step, useSound);
+};
+
+main(process.argv.splice(2));
