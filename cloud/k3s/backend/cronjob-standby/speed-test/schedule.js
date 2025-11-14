b1d0808aac71ab2b8a3378511a44fc735665b890
jobscale <jobscale@users.noreply.github.com>
2025-11-14 10:18:29 +0900

    k3d removed

--- cloud/k3s/backend/cronjob-standby/speed-test/schedule.js
+++
@@ -1,7 +0,0 @@
-const logger = console;
-const jst = [2, 6, 9, 10, 14, 18, 21, 22];
-Promise.all(jst.map(
-  v => Promise.resolve(v - 9 - 1)
-  .then(v => v < 0 ? v + 24 : v)
-))
-.then(list => logger.info(list.join(',')));
