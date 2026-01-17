10b3eeddee26df5108e99472bae65ecda358a5d4
jobscale <jobscale@users.noreply.github.com>
2026-01-17 19:06:19 +0900

    tampermonkey/notification updated

--- tampermonkey/notification.js
+++ tampermonkey/notification.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Notification Message
 // @namespace    http://tampermonkey.net/
-// @version      2025-03-18
+// @version      2026-01-17
 // @description  try to take over the world!
-// @author       You
+// @author       jobscale
 // @match        https://teams.microsoft.com/v2/*
 // @match        https://outlook.office.com/mail/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
@@ -15,39 +15,49 @@ setTimeout(() => {
   logger.info('TRIGGER');
   const opts = { num: 0 };
 
-  const observer = new MutationObserver(() => {
+  const notify = ({ organization, text }) => {
+    fetch('https://jsx.jp/api/slack', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        icon_emoji: ':anchor:',
+        username: organization,
+        text,
+        channel: 'push',
+      }),
+    });
+    fetch('https://jsx.jp/api/webPush', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: organization,
+        body: text,
+      }),
+    });
+  };
+
+  const action = () => {
     logger.info('ACTION');
     const num = [
       ...document.querySelectorAll('.fui-Badge'),
       ...document.querySelectorAll('.WIYG1.Mt2TB'),
     ].reduce((accel, el) => accel + (Number.parseInt(el.textContent, 10) || 0), 0) || 0;
 
-    const organization = document.querySelector('[id^="idna-me"]').textContent;
+    const organization = document.querySelector('[id^="idna-me"]')?.textContent || 'Unknown';
     const appName = window.location.href.match('teams') ? 'Teams' : 'Other';
     const text = `${organization} ${appName} (${num}) notification`;
     logger.info(text);
-    if (num > opts.num) {
-      fetch('https://jsx.jp/api/slack', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          icon_emoji: ':anchor:',
-          username: organization,
-          text,
-          channel: 'push',
-        }),
-      });
-      fetch('https://jsx.jp/api/webPush', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          title: organization,
-          body: text,
-        }),
-      });
-    }
+    if (num > opts.num) notify({ organization, text });
     opts.num = num;
-  });
+  };
 
+  const handler = () => {
+    requestAnimationFrame(() => {
+      clearTimeout(opts.id);
+      opts.id = setTimeout(action, 200);
+    });
+  };
+
+  const observer = new MutationObserver(handler);
   observer.observe(document.body, { childList: true, subtree: true });
 }, 33000);
